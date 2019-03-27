6fd26b8b6f5698e63521ede401d782233b619ed3
mkobilov <kobilovm@gmail.com>
2019-03-27 23:07:16 +0300

    Ring, arrow, background, start button changes
    Ring is changed to 2d completely
    Background added(will be changed to smth more fitting)
    Added rotatable arrow
    Start button added

---
+++ unityfiles/Assets/Script/Arrow.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Arrow : MonoBehaviour
+{
+    public float length;
+    private Camera cam;
+    Vector3 defaultPos;
+    private  Quaternion defaultQ;
+    private float PI = 3.1415926535897931f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateArrow();
+        cam = Camera.main;
+        //Define basic quaternion position of the arrow
+        defaultQ = gameObject.transform.rotation;
+        //Define basic vector position of the arrow
+        defaultPos = new Vector3(0, length + length / 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        EventManager.OnClicked += RemoveArrow;
+        EventManager.MClicked += ChangeArrowPos;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnClicked -= RemoveArrow;
+        EventManager.MClicked -= ChangeArrowPos;
+    }
+    private void RemoveArrow() {
+        Debug.Log("ArrowDestroyed");
+        UnityEngine.Object.Destroy(this.gameObject);
+    }
+    void ChangeArrowPos() {
+        Event curEvent = Event.current;
+        Vector3 point = new Vector3();
+        Vector2 mousePos = new Vector2();
+        //Define mouse position relative to UI in pixels
+        mousePos.x = curEvent.mousePosition.x;
+        mousePos.y = curEvent.mousePosition.y;
+        //Define vector connecting camera and mouse position
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        //Set z coordinate of vector to 0 because we are in OXY plane,
+        //Otherwise angles will be counted in OXYZ
+        point.z = 0;
+
+        float angle = 180f - Vector3.Angle(point, defaultPos);
+        //Vecor3.Angle return abs(angle), so if angle is negative changes are required,
+        //because of quaternion usage
+        if (point.x > 0) {
+            angle = -angle;
+        }
+        //Set rotation quaternion around Oz and change the rotation angle of the object
+        Quaternion rotQ = new Quaternion(0, 0,
+                                    (float)Math.Sin(PI * (angle/180)/2),
+                                    (float)Math.Cos(PI * (angle / 180) / 2));
+        gameObject.transform.rotation =  defaultQ * rotQ;
+
+    }
+    private void CreateArrow() {
+
+        float width;
+
+        width = (GameObject.Find("Ring").GetComponent<Ring>().segmentRadius)/10;
+
+        //Set vertices
+        Vector3[] vertices = new Vector3[7];
+
+        vertices[0] = new Vector3(-width, 0, 0);
+        vertices[1] = new Vector3(width, 0, 0);
+
+        vertices[2] = new Vector3(-width, length, 0);
+        vertices[3] = new Vector3(width, length, 0);
+
+        vertices[4] = new Vector3(-2*width, length, 0);
+        vertices[5] = new Vector3(2*width, length, 0);
+
+        vertices[6] = new Vector3(0, length + length/5, 0);
+
+        //Draw triangles
+        int[] triangles = new int[] {2, 1 ,0, 2 ,3, 1, 6, 5 , 4};
+
+        //Set mesh
+        Mesh mesh = new Mesh();
+        MeshFilter mFilter = GetComponent<MeshFilter>() as MeshFilter;
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateBounds();
+
+        mFilter.mesh = mesh;
+    }
+}
---
+++ unityfiles/Assets/Script/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public delegate void ClickAction();
+    public static event ClickAction OnClicked;
+
+    public delegate void MouseClickAction();
+    public static event MouseClickAction MClicked;
+
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "Start"))
+        {
+            if (OnClicked != null)
+                OnClicked();
+        }
+
+        if (Input.GetKeyDown("mouse 0")) {
+            if (MClicked != null)
+                MClicked();
+        }
+            
+    }
+}
