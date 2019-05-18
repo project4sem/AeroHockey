bc48a39f62f32379eb372093af9896a8a28f836a
Yauheni Sharamed <SHaramed.EI@phystech.edu>
2019-05-19 00:11:20 +0300

    hot fix

--- physics/movecalcVS/hit.cpp
+++ physics/movecalcVS/hit.cpp
@@ -83,9 +83,9 @@ extern "C"
 
 		printf("vx = %lf   vy = %lf\n", current.vx, current.vy);
 
-		double S = m * (k + 1) * current.vy;
+		double S = m * (k + 1) * fabs(current.vy);
 
-		sgn_before = current.vx + current.w * r_hit;
+		sgn_before = current.vx - current.w * r_hit;
 
 		ret_hit.x = current.x;
 		ret_hit.y = current.y;
@@ -94,16 +94,16 @@ extern "C"
 		ret_hit.vy = hit_vy();
 		ret_hit.w  = hit_w(S);
 
-		sgn_after = ret_hit.vx + ret_hit.w * r_hit;
+		sgn_after = ret_hit.vx - ret_hit.w * r_hit;
 
 		//printf("vx = %lf   vy = %lf\n", ret_hit.vx, ret_hit.vy);
 
 		if ( sgn_before * sgn_after <= 0 && mu != 0)
 		{
 			printf("Pereschet\n");
-			double c = (current.vx - r_hit * current.w) / 2 * m / mu;
-			ret_hit.vx = hit_vx(-c);
-			ret_hit.w  = hit_w(-c);
+			double c = fabs(current.vx - r_hit * current.w) / 2 * m / mu;
+			ret_hit.vx = hit_vx(c);
+			ret_hit.w  = hit_w(c);
 		}
 	//	printf("vx = %lf   vy = %lf\n", ret_hit.vx, ret_hit.vy);
 		ret_hit = coord_trans_back(-obj.phi, -obj.vx , -obj.vy , ret_hit);
